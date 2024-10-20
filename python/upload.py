82266b1b0815062f7c8cfbe621ab31a479cba223
Anirudh Badri Narayanan <anirudh.b.narayanan@gmail.com>
2024-10-20 21:40:41 +0530

    CSV Changes

--- python/upload.py
+++ python/upload.py
@@ -1,18 +1,37 @@
+import csv
 import mysql.connector as sql
 
 def process_upload(file_name):
     f=open(file_name,'r')
+    r=csv.reader(f)
     mycon=sql.connect(host='localhost',user='root',password='1234',database='hcm')
     cur=mycon.cursor()
-    d=f.readlines()
     k=['first_name','last_name','date_of_birth','date_of_joining','manager_id','date_of_leaving','status_code','marital_status_code','phone_number']
-    for i in d:
+    for i in r:
         l1=[]
         ind=[]
-        i=i.strip()
-        l=i.split(',')
+        l=i
         l[0]=l[0].strip()
         l[1]=l[1].strip()
+        lk=l[2].split('-')
+        s=''
+        for i in range(len(lk)-1,-1,-1):
+            if i==0:
+                s+=lk[i]
+            else:
+                s+=lk[i]
+                s+='-'
+        l[2]=s
+        print(l[2])
+        ls=l[3].split('-')
+        s1=''
+        for i in range(len(ls)-1,-1,-1):
+            if i==0:
+                s1+=ls[i]
+            else:
+                s1+=ls[i]
+                s1+='-'
+        l[3]=s1
         l[6]=int(l[6])
         l[7]=int(l[7])
         if l[8]!='':
